e64bb7eeed6cbfcce17927b323fde56231d9f016
Yurok <predbannikov.yurii@gmail.com>
2021-07-15 11:07:04 +0300

    Сумма чисел

--- codewars.com.cpp
+++ codewars.com.cpp
@@ -3,10 +3,32 @@
 
 #include "codewars.com.h"
 
-using namespace std;
+int get_sum2(int a, int b)
+{
+	int sum = a;
+	if (a < b) {
+		for (int i = a + 1; i <= b; i++) {
+			sum += i;
+		}
+	}
+	else {
+		for (int i = a - 1; i >= b; i--) {
+			sum += i;
+		}
+	}
+
+	return sum;
+}
+
+// Быстрая реализация без цикла
+int get_sum(int a, int b) {
+	int n = (a > b? a - b: b - a) + 1;
+	return n * (a + b) / 2;
+}
+
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	std::cout << get_sum(5, -1);
 	return 0;
 }
