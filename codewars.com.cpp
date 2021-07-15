658645099fb6c4d84587b556934e2f35218f66dc
Yurok <predbannikov.yurii@gmail.com>
2021-07-15 09:50:54 +0300

    first commit

---
+++ codewars.com.cpp
@@ -0,0 +1,12 @@
+﻿// codewars.com.cpp : Defines the entry point for the application.
+//
+
+#include "codewars.com.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
