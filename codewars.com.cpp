a4314d457f407b2eabbff043a0848255dc49e897
Yurok <predbannikov.yurii@gmail.com>
2021-07-18 17:36:22 +0300

    all the natural numbers below 10

--- codewars.com.cpp
+++ codewars.com.cpp
@@ -87,13 +87,26 @@ public:
 	}
 };
 
+/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is negative, return 0(for languages that do have them)*/
+int solution(int number)
+{
+	int sum = 0;
+	for (int n = 3; n < number; n++) {
+		if ((n % 3 == 0) || (n % 5 == 0))
+			sum += n;
+	}
+	return sum;
+}
 
 int main()
 {
-	Same s;
-	std::vector<int> a{ 121, 144, 19, 161, 19, 144, 19 };
-	std::vector<int> b{ 14641, 20736, 361, 25921, 361, 20736, 361, 121 };
-	std::cout << s.comp(a, b) << std::endl;
+	std::cout << solution(10) << std::endl;
+	//Same s;
+	//std::vector<int> a{ 121, 144, 19, 161, 19, 144, 19 };
+	//std::vector<int> b{ 14641, 20736, 361, 25921, 361, 20736, 361, 121 };
+	//std::cout << s.comp(a, b) << std::endl;
 	//std::cout << digital_root(167346);
 	//std::pair<int, int> pair = two_sum({ 1, 2, 3 }, 4);
 	std::cout << " ";
